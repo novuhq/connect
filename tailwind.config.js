b4ec437398e2be78e783fccb56b4a9187ce20a7a
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-04-14 18:35:22 +0300

    feat: add font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ module.exports = {
     fontFamily: {
       sans: ['Brother-1816', ...defaultTheme.fontFamily.sans],
     },
+    fontSize: {
+      12: '12px',
+      14: '14px',
+      16: '16px',
+      18: '18px',
+      20: '20px',
+      26: '26px',
+      28: '28px',
+      36: '36px',
+      40: '40px',
+      48: '48px',
+      56: '56px',
+      64: '64px',
+      96: '96px',
+    },
     colors: ({ colors }) => ({
       inherit: colors.inherit,
       current: colors.current,
@@ -38,6 +53,10 @@ module.exports = {
         11: '#F5F5F5',
       },
     }),
+    backgroundImage: {
+      'button-prinary-gradient': 'linear-gradient(257.22deg, #00A3FF 11.98%, #802CFF 92.64%)',
+      ...defaultTheme.backgroundImage,
+    },
     screens: {
       '2xl': { max: '1919px' },
       xl: { max: '1535px' },
@@ -45,6 +64,17 @@ module.exports = {
       md: { max: '1023px' },
       sm: { max: '767px' },
       xs: { max: '359px' },
+    },
+    extend: {
+      lineHeight: {
+        denser: '1.125',
+      },
+      spacing: {
+        18: '4.5rem',
+        22: '5.75rem',
+        30: '7.5rem',
+        34: '8.5rem',
+      },
     },
   },
   plugins: [require('tailwindcss-safe-area')],
