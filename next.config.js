e57422ac0a896ca0a45ffea6d9e06f97895bc2a2
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-04-17 20:24:15 +0300

    chore: font caching

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ module.exports = {
   experimental: {
     appDir: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/fonts/*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.inline.svg$/,
