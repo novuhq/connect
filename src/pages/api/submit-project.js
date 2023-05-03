33c4d5632211e8815cc614e183908062c7949ae3
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-05-03 19:26:16 +0300

    fix: minor changes

--- src/pages/api/submit-project.js
+++ src/pages/api/submit-project.js
@@ -1,16 +1,6 @@
-import { getServerSession } from 'next-auth/next';
-
-import { authOptions } from 'pages/api/auth/[...nextauth]';
-
 import prisma from '~/prisma/client';
 
 export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions);
-
-  if (!session) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
   const { method } = req;
 
   if (method !== 'POST') {
